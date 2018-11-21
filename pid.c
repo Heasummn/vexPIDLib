80f77726d2226650d24cf1f0fecb1a7e41ee86ad
Hassam <hassamuddin20@gmail.com>
2018-11-20 22:02:37 -0600

    Update pid.c
    Update code to match current PID controller

--- pid.c
+++ pid.c
@@ -5,26 +5,32 @@ typedef struct {
 	float lastActual;
 	float errorSum;
 
+	float lastD;
+	float maxOutput;
+	float minOutput;
+
+	float maxIntegral;
+
 	bool run;
 
 } pidController;
 
-void new_pid(pidController& pid, float kp, float ki, float kd) {
+void new_pid(pidController& pid, float kp, float ki, float kd, float max, float min) {
 	pid.kp = kp;
 	pid.ki = ki;
 	pid.kd = kd;
 	pid.run = false;
 	pid.errorSum = 0;
+	pid.maxOutput = max;
+	pid.minOutput = min;
 }
 
-
-// WIP
-void get_output(pidController pid, float actual, float target) {
+float get_output(pidController pid, float actual, float target) {
 	float pOutput = 0;
 	float iOutput = 0;
 	float dOutput = 0;
 
-	float error = actual - target;
+	float error = target - actual;
 
 	// simple P term
 	pOutput = pid.kp * error;
@@ -37,7 +43,25 @@ void get_output(pidController pid, float actual, float target) {
 
 	dOutput = -pid.kd * (actual - pid.lastActual);
 	pid.lastActual = actual;
+	pid.lastD = dOutput;
 
 	iOutput = pid.ki * (pid.errorSum);
+	if(iOutput > pid.maxIntegral) {
+		iOutput = pid.maxIntegral;
+	} else if(iOutput < -pid.maxIntegral) {
+		iOutput = -pid.maxIntegral;
+	}
+
+	float output = pOutput + iOutput + dOutput;
+
+	if(output > pid.maxOutput) {
+		output = pid.maxOutput;
+	} else if(output < pid.minOutput) {
+		output = pid.minOutput;
+	}
+
+	pid.errorSum += error;
+
+	return output;
 
 }
